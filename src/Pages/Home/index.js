3029cb0f6c58b6c6cc155c0b547dddeae29cb926
Sijan Maharjan <sijanpr07@gmail.com>
2026-04-29 14:21:31 +0545

    Update Message

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,14 +3,8 @@ const Home = ()=>{
     return(
         <>
         <div className="homeBannerSection">
-            <div className="row">
-                <div className="col-md-3">
-                </div>
-                    <div className="col-md-7">
-                    </div>
-                
-            </div>
-            </div>
+            
+        </div>
         </>
     )
 }
